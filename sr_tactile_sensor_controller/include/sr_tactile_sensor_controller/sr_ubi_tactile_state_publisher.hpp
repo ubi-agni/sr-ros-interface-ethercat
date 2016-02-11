8ab0318546ec34ea18a292aab7769ee4dd45ea99
Guillaume Walck <gwalck@techfak.uni-bielefeld.de>
2016-02-11 20:20:26 +0100

    Added support for calibration of the sensors, fall back to raw if no or partial calibration given

--- sr_tactile_sensor_controller/include/sr_tactile_sensor_controller/sr_ubi_tactile_state_publisher.hpp
+++ sr_tactile_sensor_controller/include/sr_tactile_sensor_controller/sr_ubi_tactile_state_publisher.hpp
@@ -16,6 +16,11 @@
 #include <sr_tactile_sensor_controller/sr_tactile_sensor_publisher.hpp>
 #include <realtime_tools/realtime_publisher.h>
 #include <tactile_msgs/TactileState.h>
+#include <sr_utilities/calibration.hpp>
+#include <sr_utilities/thread_safe_map.hpp>
+#include <boost/smart_ptr.hpp>
+#include <boost/shared_ptr.hpp>
+#include <sr_robot_lib/sr_joint_motor.hpp>
 
 namespace controller
 {
@@ -23,11 +28,23 @@ namespace controller
 class SrUbiTactileStatePublisher: public SrTactileSensorPublisher
 {
 public:
+
+  
   SrUbiTactileStatePublisher(std::vector<tactiles::AllTactileData>* sensors, double publish_rate, ros::NodeHandle nh_prefix, std::string prefix)
         : SrTactileSensorPublisher(sensors, publish_rate, nh_prefix, prefix) {}
   virtual void init();
   virtual void update(const ros::Time& time, const ros::Duration& period);
+  
+  
+  /// A temporary calibration for a given sensor.
+  boost::shared_ptr<shadow_robot::JointCalibration> calibration_tmp_;
+  
 
+protected: 
+  shadow_joints::CalibrationMap read_tactile_calibration();
+  /// The map used to calibrate each tactile.
+  shadow_joints::CalibrationMap calibration_map_;
+  
 private:
 
   typedef realtime_tools::RealtimePublisher<tactile_msgs::TactileState> UbiPublisher;
@@ -36,6 +53,9 @@ private:
   typedef boost::shared_ptr<MidPublisher > MidPublisherPtr;
   typedef realtime_tools::RealtimePublisher<tactile_msgs::TactileState> ProxPublisher;
   typedef boost::shared_ptr<ProxPublisher > ProxPublisherPtr;
+  
+  
+    
   UbiPublisherPtr ubi_realtime_pub_;
   MidPublisherPtr mid_realtime_pub_;
   ProxPublisherPtr prox_realtime_pub_;
