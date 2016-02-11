c241da3a08055a6dabacd3ee987573a248239d92
Guillaume Walck <gwalck@techfak.uni-bielefeld.de>
2016-02-11 20:19:18 +0100

    Permit multiple publishers for a tactile sensor (ubi wants to have both old and new tactile messages)

--- sr_tactile_sensor_controller/include/sr_tactile_sensor_controller/sr_tactile_sensor_controller.hpp
+++ sr_tactile_sensor_controller/include/sr_tactile_sensor_controller/sr_tactile_sensor_controller.hpp
@@ -70,7 +70,7 @@ protected:
   ros::NodeHandle nh_prefix_;
   std::string prefix_;
   bool initialized_;
-  boost::shared_ptr<SrTactileSensorPublisher> sensor_publisher_;
+  std::vector<boost::shared_ptr<SrTactileSensorPublisher> > sensor_publishers_;
 };
 
 }  // namespace controller
